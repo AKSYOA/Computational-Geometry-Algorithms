fb7da8f467be7778e5f1b72279c990babd929f40
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2022-11-22 21:59:58 +0200

    Implemented Extreme Points Algorithm

--- CGUI-master/CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
+++ CGUI-master/CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
@@ -11,6 +11,41 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+            for (int i = 0; i < points.Count; i++)
+            {
+                bool isRemoved = false;
+                for (int j = 0; j < points.Count; j++)
+                {
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        for (int l = 0; l < points.Count; l++)
+                        {
+                            if (j != i && k != i && l!= i)
+                            {
+                                Enums.PointInPolygon insideTriangleTest = HelperMethods.PointInTriangle(points[i], points[j], points[k], points[l]);
+                                if (insideTriangleTest.Equals(Enums.PointInPolygon.Inside) || insideTriangleTest.Equals(Enums.PointInPolygon.OnEdge))
+                                {
+                                    points.RemoveAt(i);
+                                    isRemoved = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (isRemoved)
+                            break;
+                
+                    }
+                    if (isRemoved)
+                        break;
+                }
+                if (isRemoved)
+                    i--;
+            
+            }   
+
+
+            outPoints = points;
+
 
         }
 
