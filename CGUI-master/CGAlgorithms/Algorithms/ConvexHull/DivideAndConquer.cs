8c8a9fce8a73321d5a26452440e9f2e5907d060e
khalid-debugg <khaled20191700225@cis.asu.edu.eg>
2022-12-07 17:23:35 +0200

    implemented divide function

--- CGUI-master/CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
+++ CGUI-master/CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
@@ -1,9 +1,6 @@
 ﻿using CGUtilities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CGAlgorithms.Algorithms.ConvexHull
 {
@@ -11,7 +8,32 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+              
+        }
 
+        public static List<Point> Divide(List<Point> divisiblePoints) {
+
+            if (divisiblePoints.Count <= 1)
+            {
+                return divisiblePoints;
+            }
+            List<Point> rightPoints = new List<Point>();
+            List<Point> leftPoints = new List<Point>();
+
+            for (int i = 0; i < divisiblePoints.Count/2; i++)
+            {
+                rightPoints.Add(divisiblePoints[i]);
+            }
+            for (int i = divisiblePoints.Count / 2; i < divisiblePoints.Count; i++) { 
+
+                leftPoints.Add(divisiblePoints[i]);
+            }
+            return Conquer(rightPoints, leftPoints);
+        }
+        public static List<Point> Conquer(List<Point> rightPoints, List<Point> leftPoints)
+        { 
+        
+        
         }
 
         public override string ToString()
--- README.md
+++ README.md
@@ -9,4 +9,4 @@
 - Convex Hull Algorithm 4 [Quick Hull] - Omar
 - ~~Convex Hull Algorithm 5 [Graham's Algorithm] - Ahmed~~
 - ~~Convex Hull Algorithm 6 [Incremental] - Youssef~~
-- Convex Hull Algorithm 7 [Divide & Conquer]
+- Convex Hull Algorithm 7 [Divide & Conquer] - Khalid
